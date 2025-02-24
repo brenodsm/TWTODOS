b43ce9d83626ec39065b9fa80bad431d429f8545
Breno dos Santos Moreira <breno.dsmnb@gmail.com>
2025-02-24 17:09:05 -0300

    feat: criar pagina de exibição de tarefas

--- app/settings.py
+++ app/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# Para incluir arquivos estáticos de cada app
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Para arquivos estáticos no diretório base do projeto
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
 
-def home(request):
-    return render(request,'home.html')
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request,"todo_list.html",{"todo": todos})
     
